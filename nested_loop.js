d110fe7514938a67eb5831f636c94963af6b83c0
Ider <ider@Iders-MacBook-Pro.local>
2023-11-13 15:47:39 +0800

    nested loop medium 3

--- nested_loop.js
+++ nested_loop.js
@@ -88,14 +88,14 @@
 // }
 
 // 3)
-console.log(dollars)
-for (let i = 0; i < 5; i++) {
+let n = 5;
+for (let i = 0; i < n; i++) {
   let sum = "";
-  
-  for (let j = 0; j < 5; j++) {
-    sum += " ";
-    sum += "$";
-   
+  for (let j = 0; j < i; j++) {
+    sum = sum + " ";
+  }
+  for (let k = 0; k < n - i; k++) {
+    sum = sum + "$";
   }
   console.log(sum);
 }
