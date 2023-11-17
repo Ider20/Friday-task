8aea950c4dd4052ccddc8016fcb38c5437de8c0f
Ider <ider@Iders-MacBook-Pro.local>
2023-11-17 14:59:52 +0800

    dynamicsearch

--- DOM_colorfulCircle.js
+++ DOM_colorfulCircle.js
@@ -1,19 +1,18 @@
 let parent = document.getElementById("inner_circle");
 let numbers = document.getElementById("circle_input");
-let array = [];
+const array = [];
 function circle() {
   for (let i = 0; i < numbers.value; i++) {
     generateCircle = document.createElement("div");
     parent.appendChild(generateCircle);
     generateCircle.classList.add("circle");
+    array.push(generateCircle);
   }
-  array = document.getElementsByClassName("circle");
   console.log(array);
 }
-
 function reEnter() {
-  for (i = 0; i < numbers.value; i++) {
-    array.pop[i];
+  for (i = 0; i < array.length; i++) {
+    parent.removeChild(array[i]);
   }
-  parent = array;
+  console.log("deleting", array.length);
 }
